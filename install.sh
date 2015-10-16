5597b92f890acd35de86c4bb50c3d5a53c48047c
George Broughton <broughton_g182@hotmail.com>
2015-10-16 16:58:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
-sudo apt-get install python-keybinder
-sudo apt-get install python-xlib
-sudo apt-get install python-pip
+sudo apt-get install python-keybinder python-xlib python-pip
 sudo pip install ewmh
