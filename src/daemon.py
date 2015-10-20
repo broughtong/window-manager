7735d209bfe83919112b1c653bcfc21b31f4a7eb
George Broughton <broughton_g182@hotmail.com>
2015-10-20 14:48:43 +0100

    Create daemon.py

---
+++ src/daemon.py
@@ -0,0 +1,62 @@
+import keybinder
+import gtk
+
+from resize import *
+from systemtray import * 
+
+class wn_daemon():
+
+	def __init__(self):
+
+		self.keystring = "<Ctrl>F1"
+		self.bind()
+
+		self.window = None
+
+		self.systemtray = wn_system_tray()
+
+	def cb_hotkey(self, e):
+
+		if self.window == None:
+
+			self.window = wn_resize()
+
+		else:
+
+			if self.window.isOpen == False:
+
+				self.window = wn_resize()	
+
+	def bind(self):
+
+		try:
+			if keybinder.bind(self.keystring, self.cb_hotkey, None):
+
+				pass
+
+			else:
+
+				raise KeyError('Failed to bind')
+
+		except KeyError as e:
+
+			md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "Failed to bind key, check that it is not already being used")
+			md.set_position(gtk.WIN_POS_CENTER)
+			md.run()
+			md.destroy()
+
+	def unbind(self):
+
+		try:
+
+			keybinder.unbind(self.keystring)
+
+		except:
+
+			pass
+
+	def rebind(self, newKeyString):
+
+		self.unbind()
+		self.keystring = newKeyString
+		self.bind()
