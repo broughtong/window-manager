3adba276e40edc90001a15525ed18964fe81b122
George Broughton <broughton_g182@hotmail.com>
2015-10-20 14:51:39 +0100

    Create about.py

---
+++ src/about.py
@@ -0,0 +1,12 @@
+import gtk
+
+class wn_about():
+
+	def __init__(self):
+
+		self.aboutdialog = gtk.AboutDialog()
+		self.aboutdialog.set_position(gtk.WIN_POS_CENTER)
+		self.aboutdialog.set_name("Window Manager")
+		self.aboutdialog.set_version("0.01")
+		self.aboutdialog.set_comments("Desktop Window Manager")
+		self.aboutdialog.set_authors(["Written by\n\nGeorge Broughton\nPeter Lightbody"])
